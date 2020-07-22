d75788bba0b020e5b4dfdc85c6e7afbc5dc93922
Kwangsun Noh <nohkwangsun@gmail.com>
2020-07-23 08:32:24 +0900

    make form

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-server = require("http").createServer((req, res) => {
-    console.log("hello")
-    res.end("hello world!322");
+require("http").createServer((req, res) => {
+    const getMemo = (i) => `
+        <div>
+            <input type="button" value="remove" onclick="alert("remove01") /> 
+            ${new Date().toLocaleString()} <br />
+            memo ${i} ssjfkjdslkfjadlfkklkd;l;lkajdfkljadslfkaksadjkfkdaslfjdakslfjdsklfjadslfk
+            dfjklaalkasdfklasdjflkadjkldsaj
+            adsjklfjaslfklakdlsjfl;dks<p />
+        </div>
+        `;
+    const memolist = [1,2,3,4,5,6].map(i => getMemo(i)).join("")
+    const writeform = `
+        <div>
+            <form action="/save" method="post">
+                <input type="textarea" name=memo placeholder="write new memo" />
+                <input type="button" value="save" />
+            </form>
+        </div>
+        `;
+    const html = `
+        <html>
+            <head><title>ksmemo</title></head>
+            <body>
+                ${writeform}
+                ${memolist}
+            </body>
+        </html>
+        `;
+    
+    res.writeHead(200);
+    res.end(html);
+
 }).listen(3000);
