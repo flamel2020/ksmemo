daa9aec1ce4720557db56968e06193b9abcdba82
Kwangsun Noh <nohkwangsun@gmail.com>
2020-07-29 06:26:07 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+#docker rm -f ksmemo
+#docker pull docker.pkg.github.com/flamel2020/ksmemo/image:latest
+#docker run --name ksmemo -p 3000:3000 -d docker.pkg.github.com/flamel2020/ksmemo/image:latest
+
 docker run -d --name ksmemo -p 3000:3000 ksmemo
