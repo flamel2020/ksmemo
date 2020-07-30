d5a29818bdef3577e8254b23b2be2feb4a97d494
Kwangsun Noh <nohkwangsun@gmail.com>
2020-07-31 01:05:21 +0900

    update watcher

--- watcher.js
+++ watcher.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-  
+
 const qs = require("querystring")
 const fs = require("fs");
 const { exec } = require("child_process");
@@ -9,46 +9,46 @@ const log = (msg) => {console.log(` .. ${cur()} ${msg}`)}
 const logerr = (msg) => {console.err(` .. ${cur()} ${msg}`)}
 
 require("http").createServer((req, res) => {
-        console.log(`client: ${req.url} at ${cur()}`)
-        if (req.method === "POST") {
-                try {
-                        let body = ""
-                        req.on("data", (data) => body += data )
-                        req.on("end", () => {
-                                const data = qs.parse(body)
-                                const payload = JSON.parse(data.payload)
-                                log(`action: ${payload.action}`)
-                                log(`full_name: ${payload.repository.full_name}`)
-                                log(`updated_at: ${payload.repository.updated_at}`)
-                                log(`url: ${payload.package.registry.url}`)
-                                log(`version: ${payload.package.package_version.version}`)
+console.log(`client: ${req.url} at ${cur()}`)
+    if (req.method === "POST") {
+        let body = ""
+        req.on("data", (data) => body += data )
+        req.on("end", () => {
+            try {
+                const data = qs.parse(body)
+                const payload = JSON.parse(data.payload)
+                log(`action: ${payload.action}`)
+                log(`full_name: ${payload.repository.full_name}`)
+                log(`updated_at: ${payload.repository.updated_at}`)
+                log(`url: ${payload.package.registry.url}`)
+                log(`version: ${payload.package.package_version.version}`)
 
-                                exec("./restart.sh", (error, stdout, stderr) => {
-                                        if (error) {
-                                                log(`error: ${error.message}`);
-                                                res.writeHead(500);
-                                                res.end(`UPDATE ERROR : ${error.message}`)
-                                                return;
-                                        }
-                                        if (stderr) {
-                                                log(`stderr: ${stderr}`);
-                                                res.writeHead(500);
-                                                res.end(`UPDATE ERROR : ${stderr}`)
-                                                return;
-                                        }
-                                        log(`stdout: ${stdout}`);
-                                        res.writeHead(200);
-                                        res.end("RESTART")
-                                })
-                        })
-                } catch (e) {
-                        logerr(e);
-                        logerr(e.message);
+                exec("./restart.sh", (error, stdout, stderr) => {
+                    if (error) {
+                        log(`error: ${error.message}`);
                         res.writeHead(500);
-                        res.end(`Internal Error : ${e.message}`);
-                }
-        } else {
-                res.writeHead(404);
-                res.end("Not Found");
-        }
+                        res.end(`UPDATE ERROR : ${error.message}`)
+                        return;
+                    }
+                    if (stderr) {
+                        log(`stderr: ${stderr}`);
+                        res.writeHead(500);
+                        res.end(`UPDATE ERROR : ${stderr}`)
+                        return;
+                    }
+                    log(`stdout: ${stdout}`);
+                    res.writeHead(200);
+                    res.end("RESTART")
+                })
+            } catch (e) {
+                logerr(e);
+                logerr(e.message);
+                res.writeHead(500);
+                res.end(`Internal Error : ${e.message}`);
+            }
+        })
+    } else {
+        res.writeHead(404);
+        res.end("Not Found");
+    }
 }).listen(3000);
